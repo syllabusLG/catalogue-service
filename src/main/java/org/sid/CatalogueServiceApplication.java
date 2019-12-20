451cef7de40523118ceabc31510e32893870cbad
andiaye <syllaba89@gmail.com>
2019-12-20 15:22:19 +0100

    first commit

---
+++ src/main/java/org/sid/CatalogueServiceApplication.java
@@ -0,0 +1,32 @@
+package org.sid;
+
+import org.sid.dao.CategoryRepository;
+import org.sid.dao.ProductRepository;
+import org.sid.entities.Category;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+@SpringBootApplication
+public class   CatalogueServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogueServiceApplication.class, args);
+    }
+
+    CommandLineRunner start(CategoryRepository categoryRepository, ProductRepository productRepository){
+        return args -> {
+            categoryRepository.deleteAll();
+            Stream.of("C1 Ordinateurs", "C2 Imprimantes").forEach(c->{
+                categoryRepository.save(new Category(c.split(" ")[0], c.split(" ")[1], new ArrayList<>()));
+            });
+            categoryRepository.findAll().forEach(System.out::println);
+
+        };
+    }
+
+
+}
---
+++ src/main/java/org/sid/entities/Category.java
@@ -0,0 +1,22 @@
+package org.sid.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Document
+@Data @NoArgsConstructor @AllArgsConstructor @ToString
+public class Category {
+    @Id
+    private String id;
+    private String name;
+    @DBRef
+    private Collection<Product> products = new ArrayList<>();
+}
