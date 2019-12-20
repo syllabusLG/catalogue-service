378cd62829137bf8bae30afe89284d2bab991f43
Syllabus <abousyllaba.ndiaye@devoteam.lu>
2019-12-20 16:48:31 +0100

    add annotation bean

--- src/main/java/org/sid/CatalogueServiceApplication.java
+++ src/main/java/org/sid/CatalogueServiceApplication.java
@@ -6,6 +6,7 @@ import org.sid.entities.Category;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import java.util.ArrayList;
 import java.util.stream.Stream;
@@ -17,6 +18,7 @@ public class   CatalogueServiceApplication {
         SpringApplication.run(CatalogueServiceApplication.class, args);
     }
 
+    @Bean
     CommandLineRunner start(CategoryRepository categoryRepository, ProductRepository productRepository){
         return args -> {
             categoryRepository.deleteAll();
