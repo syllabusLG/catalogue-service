74b370069286fd4847dcd209fad8faa4c18199c2
Syllabus <abousyllaba.ndiaye@devoteam.lu>
2019-12-27 16:21:45 +0100

    merge vé

--- src/main/java/org/sid/entities/Category.java
+++ src/main/java/org/sid/entities/Category.java
@@ -3,7 +3,6 @@ package org.sid.entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -12,13 +11,17 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 @Document
-
+@Data @NoArgsConstructor @AllArgsConstructor
 public class Category {
     @Id
     private String id;
     private String name;
     @DBRef
     private Collection<Product> products = new ArrayList<>();
+	@Override
+	public String toString() {
+		return "Category [id=" + id + ", name=" + name + "]";
+	}
 
 	
 }
