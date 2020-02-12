95fa0697daabe27d0eb4bb4992330bd8cfb8fc25
Syllabus <abousyllaba.ndiaye@devoteam.lu>
2020-02-12 15:13:22 +0100

    Cors policy

--- src/main/java/org/sid/sec/SecurityConfig.java
+++ src/main/java/org/sid/sec/SecurityConfig.java
@@ -1,14 +1,11 @@
 package org.sid.sec;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
